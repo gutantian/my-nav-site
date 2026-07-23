08e75ccf72d2a241c375136cc2f178f038a02713
tan34854 <tan34854@163.com>
2026-07-23 09:52:26 +0800

    更新导航数据：调整分类与网站，新增导航分类

--- websites.js
+++ websites.js
@@ -3,17 +3,17 @@ const websites = {
     { name: "Beszel_cpu监控", url: "http://117.72.244.30:8090/", desc: "CPU 监控面板" },
     { name: "LX_music_server", url: "http://117.72.244.30:9527/music/#", desc: "音乐服务" },
     { name: "bit密码管理", url: "https://gutantianred.dpdns.org/", desc: "密码管理器" },
-    { name: "No2_emo_url", url: "http://gutantianabc.dpdns.org", desc: "" },
-    { name: "Cloud_flare 导航", url: "https://gtdh.dpdns.org/", desc: "Cloudflare 导航站" },
-    { name: "x-ui_No2", url: "http://138.2.46.213:34576/zvY1nygvenpZyHI/", desc: "x-ui 管理面板" },
-    { name: "京东导航", url: "http://117.72.244.30:14850", desc: "京东导航首页" },
     { name: "github_pages", url: "https://gutantian.github.io/", desc: "GitHub Pages" }
   ],
   Alist_openlist: [
-    { name: "ra_alist", url: "http://107.175.0.181:15244/", desc: "Alist 网盘" },
     { name: "Alist_京东", url: "http://117.72.244.30:15244", desc: "京东 Alist" },
-    { name: "claw_cloud - openlist", url: "https://hcmrovxl.ap-southeast-1.clawcloudrun.com", desc: "Claw Cloud 网盘" },
     { name: "No.2_alist", url: "http://138.2.46.213:5244", desc: "二号 Alist" }
+  ],
+  导航: [
+    { name: "Cloud_flare 导航", url: "https://gtdh.dpdns.org/", desc: "Cloudflare 导航站" },
+    { name: "炎火导航", url: "http://103.236.97.248:51418/#/", desc: "" },
+    { name: "free-llm-api", url: "http://117.72.244.30:3001/", desc: "" },
+    { name: "New-api", url: "http://117.72.244.30:3000/pricing", desc: "" }
   ],
   常用网址: [
     { name: "科技爱好者周刊", url: "https://github.com/ruanyf/weekly", desc: "阮一峰科技周刊" },
